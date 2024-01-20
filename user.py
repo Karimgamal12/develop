e9361d75351202d42d71b9aa0f90990cf8eda8a0
Karimgamal12 <132415145+Karimgamal12@users.noreply.github.com>
2024-01-20 15:35:33 +0200

    Update user.py

--- user.py
+++ user.py
@@ -8,8 +8,8 @@ async def main():
   await app.start()
   await bot.start()
   try :
-    await app.join_chat("Source_Ze")
-    await app.join_chat("ZeSupport")
+    await app.join_chat("Software_codes")
+    await app.join_chat("P_PP_Pi")
   except :
     pass
   await name()
